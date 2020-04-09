de3b48b056e4be69b7f82f87c172f0b4ba1bf01f
Ahmed Rhman Hassani <ah.jr22@gmail.com>
2020-04-10 00:51:34 +0300

    Update MailSenderConfiger.java

--- src/main/java/com/example/demo/MailSenderConfiger.java
+++ src/main/java/com/example/demo/MailSenderConfiger.java
@@ -24,6 +24,7 @@ public class MailSenderConfiger {
         helper.setTo(to);
         helper.setText(body, true);
         javaMailSender.send(message);
+        return "Sent massage !";
     }
 
 
